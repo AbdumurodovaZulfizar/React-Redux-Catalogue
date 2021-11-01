1f4c0556062cbd781ce7b4670a620bffa65a2218
AbdumurodovaZulfizar <abdumurodovazulfizar77@gmail.com>
2021-11-01 20:19:13 +0500

    Create filter method fully

--- src/containers/FilterMethod.js
+++ src/containers/FilterMethod.js
@@ -1,30 +1,25 @@
-import { useEffect, useState } from "react";
+import {
+  useEffect,
+  useState
+} from "react";
 
 const useCategories = (category) => {
-  const [errorCategory, setErrorCategory] = useState(false);
-  const [loadingCategory, setLoadingCategory] = useState(true);
   const [categories, setCategories] = useState([]);
 
   useEffect(() => {
-    setLoadingCategory(true);
-    setErrorCategory(false);
-
     fetch('https://www.themealdb.com/api/json/v1/1/categories.php')
       .then(resp => resp.json())
       .then((result) => {
-        setLoadingCategory(false);
         setCategories(result.categories);
         console.log(result.categories);
-      }
-      ).catch((err) => {
-        if (err) {
-          setErrorCategory(true);
-        }
+      }).catch((err) => {
+        if (err) {}
       })
-    }
-  ,[category])
+  }, [category])
 
-  return {loadingCategory, categories, errorCategory}
+  return {
+    categories
+  }
 }
 
 export default useCategories;
