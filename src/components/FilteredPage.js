2c78efb0631a22fe6fc69c15ec934d1ff8c3a228
AbdumurodovaZulfizar <abdumurodovazulfizar77@gmail.com>
2021-11-01 20:20:36 +0500

    Create defoult filter UI

--- src/components/FilteredPage.js
+++ src/components/FilteredPage.js
@@ -1,6 +1,36 @@
 import React from "react";
-import { connect } from "react-redux";
-import { changeFilter } from "../actions";
-import FilterMethod from "../containers/filterMethod";
+import PropTypes from 'prop-types';
+import useCategories from "../containers/FilterMethod";
 
-const Filter = ({ })
+const FilteredPage = ({ filter, handleFilter }) => {
+  const { categories } = useCategories();
+  return (
+    <div className="category-wrapper pt-5 mt-5">
+    <select
+      name="category"
+      value={filter}
+      onChange={(e) => handleFilter(e.target.value)}
+      className="col-4 py-1"
+    >
+      {categories.map((item) => (
+        <option value={item} key={item}>
+          {item}
+        </option>
+      ))}
+    </select>
+  </div>
+  )
+}
+
+FilteredPage.propTypes = {
+  filter: PropTypes.string,
+  handleFilter: PropTypes.func,
+};
+
+FilteredPage.defaultProps = {
+  filter: 'All',
+  handleFilter: null,
+};
+
+
+export default FilteredPage;
