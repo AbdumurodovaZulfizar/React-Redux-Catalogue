e82ebb0a0ed49dea495a036acd8e3d0e7722af70
AbdumurodovaZulfizar <abdumurodovazulfizar77@gmail.com>
2021-11-01 20:19:48 +0500

    Create Router for filter

--- src/components/Router.js
+++ src/components/Router.js
@@ -1,8 +1,8 @@
 import React from 'react';
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
-// import FilterMethod from '../containers/filterMethod';
 import Header from './Header';
 import Home from './Home';
+import FilteredPage from './FilteredPage';
 
 const AppRouter = () => {
   return (
@@ -10,7 +10,7 @@ const AppRouter = () => {
     <div>
       <Header />
       <Switch>
-        {/* <Route path="/category" component={FilterMethod} /> */}
+        <Route path="/category" component={FilteredPage} />
         <Route path="/" component={Home} />
       </Switch>
     </div>
